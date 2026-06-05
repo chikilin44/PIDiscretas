8c60ca5fc604e5a588c207fd685bffa94d25a79a
Marko Andres Arcila Zapata <markus.34189@gmail.com>
2026-06-04 22:00:19 -0500

    Add route for Modulos componental

--- clientes-app-frontend/src/app/app-routing-module.ts
+++ clientes-app-frontend/src/app/app-routing-module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { Temas } from './temas/temas';
+import { Modulos } from './modulos/modulos';
 
 const routes: Routes = [
   { path: '', component: Temas },
+  { path: 'modulos', component: Modulos },
 ];
 
 @NgModule({
